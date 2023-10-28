8985f309dc488ff94883ff821974a40c18302ab7
Matthew Freire <matt@justdjango.com>
2023-10-28 11:21:21 +0100

    Update build config

--- build.zig
+++ build.zig
@@ -1,22 +1,14 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
-    // these are boiler plate code until you know what you are doing
-    // and you need to add additional options
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zig_llm",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
+    const dep_opts = .{ .target = target, .optimize = optimize };
+    _ = dep_opts;
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
+    _ = b.addModule("zig-llm", .{
+        .source_file = .{ .path = "src/main.zig" },
+        .dependencies = &.{},
+    });
 }
