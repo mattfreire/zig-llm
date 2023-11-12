643322556f181f5e6461624ef06eb11ca451c4f7
Matthew Freire <matt@justdjango.com>
2023-11-12 16:44:42 +0000

    tests, ci and fixes

--- src/openai.zig
+++ src/openai.zig
@@ -27,14 +27,55 @@ pub const Message = struct {
     content: []const u8,
 };
 
+pub const Model = struct {
+    id: []const u8,
+    object: []const u8,
+    created: u64,
+    owned_by: []const u8,
+};
+
+pub const ModelResponse = struct {
+    object: []const u8,
+    data: []Model,
+};
+
+pub const CompletionPayload = struct { model: []const u8, messages: []Message, max_tokens: ?u32, temperature: ?u8 };
+
+const OpenAIError = error{
+    BAD_REQUEST,
+    UNAUTHORIZED,
+    FORBIDDEN,
+    NOT_FOUND,
+    TOO_MANY_REQUESTS,
+    INTERNAL_SERVER_ERROR,
+    SERVICE_UNAVAILABLE,
+    GATEWAY_TIMEOUT,
+    UNKNOWN,
+};
+
+fn getError(status: std.http.Status) OpenAIError {
+    const result = switch (status) {
+        .bad_request => OpenAIError.BAD_REQUEST,
+        .unauthorized => OpenAIError.UNAUTHORIZED,
+        .forbidden => OpenAIError.FORBIDDEN,
+        .not_found => OpenAIError.NOT_FOUND,
+        .too_many_requests => OpenAIError.TOO_MANY_REQUESTS,
+        .internal_server_error => OpenAIError.INTERNAL_SERVER_ERROR,
+        .service_unavailable => OpenAIError.SERVICE_UNAVAILABLE,
+        .gateway_timeout => OpenAIError.GATEWAY_TIMEOUT,
+        else => OpenAIError.UNKNOWN,
+    };
+    return result;
+}
+
 pub const OpenAI = struct {
     base_url: []const u8 = "https://api.openai.com/v1",
     api_key: []const u8,
-    organization_id: []const u8,
+    organization_id: ?[]const u8,
     alloc: Allocator,
     headers: std.http.Headers,
 
-    pub fn init(alloc: Allocator, api_key: []const u8, organization_id: []const u8) !OpenAI {
+    pub fn init(alloc: Allocator, api_key: []const u8, organization_id: ?[]const u8) !OpenAI {
         const headers = try get_headers(alloc, api_key);
         return OpenAI{ .alloc = alloc, .api_key = api_key, .organization_id = organization_id, .headers = headers };
     }
@@ -52,7 +93,7 @@ pub const OpenAI = struct {
         return headers;
     }
 
-    pub fn get_models(self: *OpenAI) !void {
+    pub fn get_models(self: *OpenAI) anyerror![]Model {
         var client = std.http.Client{
             .allocator = self.alloc,
         };
@@ -66,11 +107,23 @@ pub const OpenAI = struct {
         try req.start();
         try req.wait();
 
-        const body = req.reader().readAllAlloc(self.alloc, 3276800) catch unreachable;
-        defer self.alloc.free(body);
+        const status = req.response.status;
+
+        if (status != .ok) {
+            return getError(status);
+        }
+
+        const response = req.reader().readAllAlloc(self.alloc, 3276800) catch unreachable;
+        defer self.alloc.free(response);
+
+        const parsed_models = try std.json.parseFromSlice(ModelResponse, self.alloc, response, .{});
+        defer parsed_models.deinit();
+
+        // TODO return this as a slice
+        return parsed_models.value.data;
     }
 
-    pub fn completion(self: *OpenAI, model: []const u8, prompt: []const u8, max_tokens: u32, temperature: u8, verbose: bool) !Completion {
+    pub fn completion(self: *OpenAI, payload: CompletionPayload, verbose: ?bool) !Completion {
         var client = std.http.Client{
             .allocator = self.alloc,
         };
@@ -78,21 +131,7 @@ pub const OpenAI = struct {
 
         const uri = std.Uri.parse("https://api.openai.com/v1/chat/completions") catch unreachable;
 
-        var system_message = .{
-            .role = "system",
-            .content = "You are a helpful assistant",
-        };
-
-        var user_message = .{
-            .role = "user",
-            .content = prompt,
-        };
-
-        const messages = [2]Message{ system_message, user_message };
-
-        var body_raw = .{ .max_tokens = max_tokens, .temperature = temperature, .model = model, .messages = messages };
-
-        const body = try std.json.stringifyAlloc(self.alloc, body_raw, .{});
+        const body = try std.json.stringifyAlloc(self.alloc, payload, .{});
         defer self.alloc.free(body);
 
         var req = try client.request(.POST, uri, self.headers, .{});
@@ -105,13 +144,82 @@ pub const OpenAI = struct {
         try req.finish();
         try req.wait();
 
+        const status = req.response.status;
+
+        if (status != .ok) {
+            return getError(status);
+        }
+
         const response = req.reader().readAllAlloc(self.alloc, 3276800) catch unreachable;
-        if (verbose) {
+        if (verbose.?) {
             log.debug("Response: {s}\n", .{response});
         }
-        // defer self.alloc.free(response);
+        defer self.alloc.free(response);
+
         const parsed_completion = try std.json.parseFromSlice(Completion, self.alloc, response, .{});
-        // defer config.deinit();
+        defer parsed_completion.deinit();
+
         return parsed_completion.value;
     }
 };
+
+test "unauthorized api key" {
+    const alloc = std.testing.allocator;
+
+    const api_key = "sk-1234567890";
+
+    var openai = try OpenAI.init(alloc, api_key, null);
+    defer openai.deinit();
+
+    const models = openai.get_models();
+
+    try std.testing.expectEqual(models, OpenAIError.UNAUTHORIZED);
+}
+
+test "get models" {
+    const alloc = std.testing.allocator;
+    var env = try std.process.getEnvMap(alloc);
+    defer env.deinit();
+
+    const api_key = env.get("OPENAI_API_KEY");
+
+    var openai = try OpenAI.init(alloc, api_key.?, null);
+    defer openai.deinit();
+
+    const models = try openai.get_models();
+
+    try std.testing.expect(models.len > 0);
+}
+
+test "completion" {
+    const alloc = std.testing.allocator;
+    var env = try std.process.getEnvMap(alloc);
+    defer env.deinit();
+
+    const api_key = env.get("OPENAI_API_KEY");
+
+    var openai = try OpenAI.init(alloc, api_key.?, null);
+    defer openai.deinit();
+
+    var system_message = .{
+        .role = "system",
+        .content = "You are a helpful assistant",
+    };
+
+    var user_message = .{
+        .role = "user",
+        .content = "Write a 1 line haiku",
+    };
+
+    var messages = [2]Message{ system_message, user_message };
+
+    var payload = CompletionPayload{
+        .model = "gpt-3.5-turbo",
+        .messages = &messages,
+        .max_tokens = 64,
+        .temperature = 0,
+    };
+    const completion = try openai.completion(payload, false);
+
+    try std.testing.expect(completion.choices.len > 0);
+}
